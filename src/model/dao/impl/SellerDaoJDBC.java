c6356c4209f01e993f630df010fb8c9b770d827b
Felipe <jorgefelipe.jf01@gmail.com>
2022-12-08 21:50:50 -0300

    findById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+	
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller obj) {
@@ -24,8 +37,39 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-
-		return null;
+		PreparedStatement st = null;//PretaredStatement é um objeto que permite montar a consulta SQL deixando os parâmetros para colocar depois
+		ResultSet rs = null; //Comando para pegar os valores do Banco de dados
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department as department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ? ");//(?)Significa que é aqui é o lugar onde depois será acrescentado o valor
+					
+					st.setInt(1, id); // (1, porque é a alteração do 1° ?)
+					rs = st.executeQuery();//executeQuery serve para executar o comando acima 
+					if(rs.next()) {
+						Department dep = new Department();
+						dep.setId(rs.getInt("DepartmentId"));
+						dep.setName(rs.getString("DepName"));
+						Seller obj = new Seller();
+						obj.setId(rs.getInt("Id"));
+						obj.setName(rs.getString("Name"));
+						obj.setEmail(rs.getString("Email"));
+						obj.setBaseSalary(rs.getDouble("BaseSalary"));
+						obj.setBirthDate(rs.getDate("BirthDate"));
+						obj.setDepartment(dep);
+						return obj;
+					}
+					return null;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
