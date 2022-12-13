26d7cbbccb8e7bb647d782e72ee305745d98136b
Felipe <jorgefelipe.jf01@gmail.com>
2022-12-13 19:03:48 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,4 @@
-package application; 
+package application;
 
 import java.util.List;
 
@@ -10,19 +10,26 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		SellerDao sellerDao = DaoFactory.createSellerDao();// Dessa forma, o programa não conhece a implementação, ele conhece somente a interface.
-		
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();// Dessa forma, o programa não conhece a implementação, ele
+															// conhece somente a interface.
+
 		System.out.println("=== TEST 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
-        System.out.println(seller);
-        
-        System.out.println("\n=== TEST 2: seller findByDepartment ====");
-        Department department = new Department(2, null);
-        List<Seller> list = sellerDao.findByDepartment(department);
-        for(Seller obj : list) {
-        	System.out.println(obj);
-        }
+		System.out.println(seller);
+
+		System.out.println("\n=== TEST 2: seller findByDepartment ====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n=== TEST 3: seller findAll ====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
